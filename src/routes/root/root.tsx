00e67e70b26c8bc349c220fd8147d23b458991e4
Alex <alex.devenrgy@gmail.com>
2024-04-10 18:06:23 +0300

    fix: add preload for video

--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,24 +1,23 @@
-import styles from './root.module.scss';
-
 import { Outlet } from 'react-router-dom';
 
 import { Header } from 'components/header';
 
+import styles from './root.module.scss';
+
 export default function Root() {
   return (
     <div className={styles.root}>
-      <video className={styles.video} autoPlay muted loop>
-        <source src='/video/bg.mp4' type='video/mp4'/>
+      <video preload='metadata' className={styles.video} autoPlay muted loop>
+        <source src='/video/bg.mp4' type='video/mp4' />
       </video>
 
       <div className={styles.wrapper}>
-        <Header/>
+        <Header />
 
         <main>
-          <Outlet/>
+          <Outlet />
         </main>
       </div>
     </div>
   );
 }
-
